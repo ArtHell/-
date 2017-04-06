2cd973c9267286ba112eda1c86550d723876b95f
vladimir <nalivaikovova@gmail.com>
2017-04-06 22:08:31 +0300

    created categories

--- Doshka/Migrations/Configuration.cs
+++ Doshka/Migrations/Configuration.cs
@@ -1,3 +1,4 @@
+using Doshka.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Doshka.Migrations
@@ -20,6 +21,64 @@ namespace Doshka.Migrations
             {
                 Id = "Admin",
                 Name = "Admin"
+            });
+
+            context.Categories.AddOrUpdate(new Category()
+            {
+                CategoryId = 1,
+                Name = "All categories"
+            }, new Category()
+            {
+                CategoryId = 2,
+                Name = "Fashion & Style"
+            }, new Category()
+            {
+                CategoryId = 3,
+                Name = "Housing"
+            }, new Category()
+            {
+                CategoryId = 4,
+                Name = "Realty"
+            }, new Category()
+            {
+                CategoryId = 5,
+                Name = "All for moms & babies"
+            }, new Category()
+            {
+                CategoryId = 6,
+                Name = "Auto & transport"
+            }, new Category()
+            {
+                CategoryId = 7,
+                Name = "Hobbies, sports & tourism"
+            }, new Category()
+            {
+                CategoryId = 8,
+                Name = "Repair and construction"
+            }, new Category()
+            {
+                CategoryId = 9,
+                Name = "Toys & hobbies"
+            }, new Category()
+            {
+                CategoryId = 10,
+                Name = "Garden"
+            }, new Category()
+            {
+                CategoryId = 11,
+                Name = "Wedding"
+            }, new Category()
+            {
+                CategoryId = 12,
+                Name = "Entertainment"
+            }, new Category()
+            {
+                CategoryId = 13,
+                Name = "Work, business and study"
+            }, new Category()
+            {
+                CategoryId = 14,
+                Name = "Service"
             });
         }
     }
