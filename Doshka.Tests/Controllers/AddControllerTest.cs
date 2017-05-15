c7da2c47dc10205bc5f19b35a6d009ea64ad4dab
ArtHell <temka19977@mail.ru>
2017-05-15 23:52:30 +0300

    added test covered Vova's code

---
+++ Doshka.Tests/Controllers/AddControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Doshka;
+using Doshka.Controllers;
+
+namespace Doshka.Tests.Controllers
+{
+    [TestClass]
+    public class AdsControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            var controller = new AdsController();
+            var result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void DeleteOutdatedAds()
+        {
+            var controller = new AdsController();
+            var result = controller.DeleteOutdatedAds();
+            Assert.AreEqual(new EmptyResult(), result);
+        }
+
+
+        [TestMethod]
+        public void Delete()
+        {
+            var controller = new AdsController();
+            var result = controller.Delete(new Random().Next());
+            Assert.AreNotEqual(
+                new EmptyResult(), 
+                new HttpStatusCodeResult(HttpStatusCode.BadRequest));
+        }
+    }
+}
